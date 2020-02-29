071cc33198a70415248adf8a495a85b29496d626
Acjal <amazingalpacaaria@gmail.com>
2020-02-29 12:23:53 -0800

    Initialize

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UGUI1
+{ 
+public class UIManager : MonoBehaviourPun
+{
+    public InputField usernameInp;
+    public Button loginBtn;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
